34057a683afdb618217318c9b71a4d07f1644e2d
liuyu <liuyu30@hisense.com>
2021-12-23 13:21:05 +0800

    解决Vue路由跳转报错

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import Router from "vue-router";
 
 Vue.use(Router);
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+}
 
 const routes = [
   {
